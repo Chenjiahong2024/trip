49388e4d9d5ed72e25826722f936729beb2c0074
4 <1>
2025-07-12 15:44:25 +0800

    Initial Commit

---
+++ trip/tripApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripApp.swift
+//  trip
+//
+//  Created by Jiahong Chen  on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct tripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
